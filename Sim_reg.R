f4105682dbe8c3202c47d679faa7bf6499e91d38
zhizuio <39630023+zhizuio@users.noreply.github.com>
2019-02-08 13:23:59 +0100

    Add files via upload

---
+++ Sim_reg.R
@@ -0,0 +1,182 @@
+
+library(tgp)
+library(doParallel)
+library(glmnet)
+library(penalizedSVM)
+library(IPFStructPenalty)
+# load the simulation functions
+source("sim.dat.R")
+#===============================
+para<-matrix(c(
+  150, 150,
+  500, 150),nrow=2,byrow=T)
+
+# this is the function to do simulation analysis
+SimAnalysis <- function(num=1, scenario=1, hete=1){
+  # generate learning dataset
+  set.seed(num)
+  repeat{
+    sim <- do.call(paste("sim",scenario,sep=""), list(p=para[hete,]))
+    foldid <- sample(rep(seq(5),length=dim(sim$X)[1]))
+    min.height <- mheight(sim$Y, foldid)
+    if(min.height<0.7) break                
+  }
+  Beta<-sim$Beta
+  p<-sim$p
+  y<-scale(sim$Y)[,]
+  x<-scale(sim$X)[,]
+  
+  # generate validation dataset
+  repeat{
+    sim_test <- do.call(paste("sim",scenario,sep=""), list(p=para[hete,]))
+    min.height_test <- mheight(sim_test$Y)
+    if(min.height_test<0.7) break                
+  }
+  y_test <- scale(sim_test$Y)[,]
+  x_test <- scale(sim_test$X)[,]
+  
+  cvm <- array(0,dim=c(6000,24))
+  methods <- c("lasso", "elastic-net", "IPF-lasso", "IPF-elastic-net", "tree-lasso", "IPF-tree")
+  for(i in 1:1){
+    fit <- IPFStructPenaltyReg(x, y, x_test, y_test, p, foldid, method=methods[i])
+    cvm[i,1:5] <- c(fit$cvm, fit$cvm_cv, fit$lambda, fit$ipf,
+                   ifelse(length(fit$alpha)==1,fit$alpha,round(fit$alpha[1],dig=2)*100+fit$alpha[2]))
+    cvm[(11+100*(i-1)):(10+100*i),1:24] <- array(as.vector(fit$pred),dim=c(100,24))
+    cvm[(1001+(sum(p)+1)*(i-1)):(1000+(sum(p)+1)*i),1:24] <- fit$Beta
+  }
+  save(cvm,file=paste("results/SimScenario", scenario, "_", hete, "_", num, ".RData", sep=""))
+}
+
+# Doing 50 simulations for the 3 scenarios with 2 cases of each
+for(num in 1:50)
+  for(scenario in 1:3)
+    for(hete in 1:2)
+      SimAnalysis(num, scenario, hete)
+
+#=========================
+# load all results of the simulation analysis and show their prediction performance
+#=========================
+B <- 50
+cvm1 <- cvm2 <- cvm3 <- cvm4 <- cvm5 <- cvm6 <- array(0,dim=c(B,6000,24))
+# two different structures of B1 & B2, b=c(0.6,0.2), p=c(150,150)
+filelist<-list.files(path="results/",pattern=paste("SimScenario1_1_*",sep=""))[1:B]
+datalist<-lapply(paste("results/",filelist,sep=""), function(x){load(x);cvm})
+for(i in 1:B) cvm1[i,,]<-datalist[[i]][,,2]
+# two different structures of B1 & B2, b=c(0.6,0.2), p=c(500,150)
+filelist<-list.files(path="results/",pattern=paste("SimScenario1_2_*",sep=""))[1:B]
+datalist<-lapply(paste("results/",filelist,sep=""), function(x){load(x);cvm})
+for(i in 1:B) cvm2[i,,]<-datalist[[i]][,,2]
+# two different un-structures of B1 & B2, b=c(0.6,0.2), p=c(150,150)
+filelist<-list.files(path="results/",pattern=paste("SimScenario2_1_*",sep=""))[1:B]
+datalist<-lapply(paste("results/",filelist,sep=""), function(x){load(x);cvm})
+for(i in 1:B) cvm3[i,,]<-datalist[[i]][,,2]
+# two different un-structures of B1 & B2, b=c(0.6,0.2), p=c(150,150)
+filelist<-list.files(path="results/",pattern=paste("SimScenario2_2_*",sep=""))[1:B]
+datalist<-lapply(paste("results/",filelist,sep=""), function(x){load(x);cvm})
+for(i in 1:B) cvm4[i,,]<-datalist[[i]][,,2]
+# two similar structures of B1 & B2, b=c(0.6,0.2), p=c(150,150)
+filelist<-list.files(path="results/",pattern=paste("SimScenario3_1_*",sep=""))[1:B]
+datalist<-lapply(paste("results/",filelist,sep=""), function(x){load(x);cvm})
+for(i in 1:B) cvm5[i,,]<-datalist[[i]][,,2]
+# two similar structures of B1 & B2, b=c(0.6,0.2), p=c(500,150)
+filelist<-list.files(path="results/",pattern=paste("SimScenario3_2_*",sep=""))[1:B]
+datalist<-lapply(paste("results/",filelist,sep=""), function(x){load(x);cvm})
+for(i in 1:B) cvm6[i,,]<-datalist[[i]][,,2]
+
+
+lab<-c("lasso","elastic","IPF-lasso","sIPFEN","tree-lasso","IPF-tree-lasso")
+x0<-rep(lab,each=B)
+x1 <-factor(x0, levels=c("lasso","IPF-lasso","elastic","sIPFEN","tree-lasso","IPF-tree-lasso"))
+# plot MSE from validation data
+layout(matrix(1:8,nrow=2,byrow=F))
+par(mar=c(1,4,4,1))
+cvm0<-cvm1
+y0<-c(cvm0[,1,1],cvm0[,2,1],cvm0[,3,1],cvm0[,4,1],cvm0[,5,1],cvm0[,6,1])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.09,0.22))
+title(ylab=expression("MSE"["val"]), line=2.2)
+title("(a) Scenario 1")
+mtext("p=(150,150)",cex=3/4,line=.3)
+cvm0<-cvm2
+y0<-c(cvm0[,1,1],cvm0[,2,1],cvm0[,3,1],cvm0[,4,1],cvm0[,5,1],cvm0[,6,1])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.09,0.22))
+title(ylab=expression("MSE"["val"]), line=2.2)
+title("(b) Scenario 1")
+mtext("p=(500,150)",cex=3/4,line=.3)
+cvm0<-cvm3
+y0<-c(cvm0[,1,1],cvm0[,2,1],cvm0[,3,1],cvm0[,4,1],cvm0[,5,1],cvm0[,6,1])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.05,0.18))
+title(ylab=expression("MSE"["val"]), line=2.2)
+title("(c) Scenario 2")
+mtext("p=(150,150)",cex=3/4,line=.3)
+cvm0<-cvm4
+y0<-c(cvm0[,1,1],cvm0[,2,1],cvm0[,3,1],cvm0[,4,1],cvm0[,5,1],cvm0[,6,1])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.05,0.18))
+title(ylab=expression("MSE"["val"]), line=2.2)
+title("(d) Scenario 2")
+mtext("p=(500,150)",cex=3/4,line=.3)
+cvm0<-cvm5
+y0<-c(cvm0[,1,1],cvm0[,2,1],cvm0[,3,1],cvm0[,4,1],cvm0[,5,1],cvm0[,6,1])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.3,0.43))
+title(ylab=expression("MSE"["val"]), line=2.2)
+title("(e) Scenario 3")
+mtext("p=(150,150)",cex=3/4,line=.3)
+cvm0<-cvm6
+y0<-c(cvm0[,1,1],cvm0[,2,1],cvm0[,3,1],cvm0[,4,1],cvm0[,5,1],cvm0[,6,1])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.3,0.43))
+title(ylab=expression("MSE"["val"]), line=2.2)
+title("(f) Scenario 3")
+mtext("p=(500,150)",cex=3/4,line=.3)
+
+par(mar=c(5,1,4,0))
+plot(1:10, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = '', xlab = '')
+legend(0,10,
+       legend = c("lasso","IPF-lasso","elastic-net","sIPF-elastic-net","tree-lasso","IPF-tree-lasso"), 
+       fill = gray.colors(6), bty="n", y.intersp=2)
+
+# plot MSE of cross-validation from learning data
+layout(matrix(1:8,nrow=2,byrow=F))
+par(mar=c(1,4,4,1))
+cvm0<-cvm1
+y0<-c(cvm0[,1,2],cvm0[,2,2],cvm0[,3,2],cvm0[,4,2],cvm0[,5,2],cvm0[,6,2])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.1,0.26))
+title(ylab=expression("MSE"["CV"]), line=2.2)
+title("(a) Scenario 1")
+mtext("p=(150,150)",cex=3/4,line=.3)
+cvm0<-cvm2
+y0<-c(cvm0[,1,2],cvm0[,2,2],cvm0[,3,2],cvm0[,4,2],cvm0[,5,2],cvm0[,6,2])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.1,0.26))
+title(ylab=expression("MSE"["CV"]), line=2.2)
+title("(b) Scenario 1")
+mtext("p=(500,150)",cex=3/4,line=.3)
+cvm0<-cvm3
+y0<-c(cvm0[,1,2],cvm0[,2,2],cvm0[,3,2],cvm0[,4,2],cvm0[,5,2],cvm0[,6,2])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.06,0.22))
+title(ylab=expression("MSE"["CV"]), line=2.2)
+title("(c) Scenario 2")
+mtext("p=(150,150)",cex=3/4,line=.3)
+cvm0<-cvm4
+y0<-c(cvm0[,1,2],cvm0[,2,2],cvm0[,3,2],cvm0[,4,2],cvm0[,5,2],cvm0[,6,2])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.06,0.22))
+title(ylab=expression("MSE"["CV"]), line=2.2)
+title("(d) Scenario 2")
+mtext("p=(500,150)",cex=3/4,line=.3)
+cvm0<-cvm5
+y0<-c(cvm0[,1,2],cvm0[,2,2],cvm0[,3,2],cvm0[,4,2],cvm0[,5,2],cvm0[,6,2])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.305,0.465))
+title(ylab=expression("MSE"["CV"]), line=2.2)
+title("(e) Scenario 3")
+mtext("p=(150,150)",cex=3/4,line=.3)
+cvm0<-cvm6
+y0<-c(cvm0[,1,2],cvm0[,2,2],cvm0[,3,2],cvm0[,4,2],cvm0[,5,2],cvm0[,6,2])
+boxplot(y0~x1,boxwex=0.7,col=gray.colors(6),xaxt="n",ylim=c(0.305,0.465))
+title(ylab=expression("MSE"["CV"]), line=2.2)
+title("(f) Scenario 3")
+mtext("p=(500,150)",cex=3/4,line=.3)
+
+par(mar=c(5,1,4,0))
+plot(1:10, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = '', xlab = '')
+legend(0,10,
+       legend = c("lasso","IPF-lasso","elastic-net","sIPF-elastic-net","tree-lasso","IPF-tree-lasso"), 
+       fill = gray.colors(6), bty="n", y.intersp=2)
+
+  
---
+++ sim.dat.R
@@ -0,0 +1,201 @@
+#================================================================================================================
+# This script includes three functions to simulate two heterogeneous data sources (covariates) with multivariate response variables.
+#
+# author: Zhi Zhao (zhi.zhao@medisin.uio.no)
+# date: 08-Feb-2019
+#================================================================================================================
+
+sim1 <- function(p=c(500,150),n=100,m=24,rho=.4,B.elem=c(0.2,0.6)){
+  b<-10
+  if(!is.na(p[2])){
+    # generate covariance matrix
+    Ap1<-matrix(rep(rho,(p[1]/b)^2),nrow=p[1]/b)
+    diag(Ap1)<-rep(1,p[1]/b)
+    Ap2<-matrix(rep(rho,(p[2]/b)^2),nrow=p[2]/b)
+    diag(Ap2)<-rep(1,p[2]/b)
+    Bp12<-matrix(rep(rho,p[1]/b*p[2]/b),nrow=p[1]/b)
+    Bp21<-matrix(rep(rho,p[1]/b*p[2]/b),nrow=p[2]/b)
+    Xsigma1<-Ap1
+    Xsigma2<-Ap2
+    Xsigma12<-Bp12
+    Xsigma21<-Bp21
+    for(i in 2:b){
+      Xsigma1<-bdiag(Xsigma1,Ap1)
+      Xsigma12<-bdiag(Xsigma12,Bp12)
+      Xsigma2<-bdiag(Xsigma2,Ap2)
+      Xsigma21<-bdiag(Xsigma21,Bp21)
+    }
+    
+    Xsigma<-rbind(cbind(Xsigma1,Xsigma12),cbind(Xsigma21,Xsigma2))
+    X<-	mvrnorm(n,mu=rep(0,p[1]+p[2]),Sigma=Xsigma)
+    X1<-X[,1:p[1]]
+    X2<-data.matrix(X[,(p[1]+1):(p[1]+p[2])] > 0) + 0
+    
+    # generate uncorrelated error term
+    esd<-diag(m)
+    e<-mvrnorm(n,mu=rep(0,m),Sigma=esd)
+    
+    ## generate beta1 matrix
+    Beta1<-matrix(0,nrow=m,ncol=p[1])
+    Beta1[,1]<-B.elem[1]
+    for(i in 1:2){
+      Beta1[((i-1)*m/2+1):(i*m/2),(1+(i-1)*2+1):(1+i*2)]<-B.elem[1]
+    }
+    for(i in 1:4){
+      Beta1[((i-1)*m/4+1):(i*m/4),(1+4*2+(i-1)*4+1):(1+4*2+i*4)]<-B.elem[1]
+    }
+    for(i in 1:8){
+      Beta1[((i-1)*m/8+1):(i*m/8),(1+2*2+4*4+(i-1)*8+1):(1+2*2+4*4+i*8)]<-B.elem[1]
+    }
+    ## generate beta2 matrix
+    Beta2<-matrix(0,nrow=m,ncol=p[2])
+    Beta2[,1]<-B.elem[2]
+    for(i in 1:3){
+      Beta2[((i-1)*m/3+1):(i*m/3),(1+(i-1)*2+1):(1+i*2)]<-B.elem[2]
+    }
+    for(i in 1:6){
+      Beta2[((i-1)*m/6+1):(i*m/6),(1+2*3+(i-1)*4+1):(1+2*3+i*4)]<-B.elem[2]
+    }
+    for(i in 1:12){
+      Beta2[((i-1)*m/12+1):(i*m/12),(1+2*3+4*6+(i-1)*8+1):(1+2*3+4*6+i*8)]<-B.elem[2]
+    }
+    Beta<-t(cbind(Beta1, Beta2))
+  }else{
+    cat("Ooops!!! Please specify 2-dim p vector, for example p=c(500,150)\n")
+  }
+  
+  Y<-X%*%Beta+e
+  return(list(Y=Y, X=X, Beta=Beta, e=e, p=p))
+}
+
+sim2 <- function(p=c(500,150),n=100,m=24,rho=.4,B.elem=c(0.2,0.6)){
+  b<-10
+  if(!is.na(p[2])){
+    # generate covariance matrix
+    Ap1<-matrix(rep(rho,(p[1]/b)^2),nrow=p[1]/b)
+    diag(Ap1)<-rep(1,p[1]/b)
+    Ap2<-matrix(rep(rho,(p[2]/b)^2),nrow=p[2]/b)
+    diag(Ap2)<-rep(1,p[2]/b)
+    Bp12<-matrix(rep(rho,p[1]/b*p[2]/b),nrow=p[1]/b)
+    Bp21<-matrix(rep(rho,p[1]/b*p[2]/b),nrow=p[2]/b)
+    Xsigma1<-Ap1
+    Xsigma2<-Ap2
+    Xsigma12<-Bp12
+    Xsigma21<-Bp21
+    for(i in 2:b){
+      Xsigma1<-bdiag(Xsigma1,Ap1)
+      Xsigma12<-bdiag(Xsigma12,Bp12)
+      Xsigma2<-bdiag(Xsigma2,Ap2)
+      Xsigma21<-bdiag(Xsigma21,Bp21)
+    }
+    
+    Xsigma<-rbind(cbind(Xsigma1,Xsigma12),cbind(Xsigma21,Xsigma2))
+    X<-	mvrnorm(n,mu=rep(0,p[1]+p[2]),Sigma=Xsigma)
+    X1<-X[,1:p[1]]
+    X2<-data.matrix(X[,(p[1]+1):(p[1]+p[2])] > 0) + 0
+    
+    # generate uncorrelated error term
+    esd<-diag(m)
+    e<-mvrnorm(n,mu=rep(0,m),Sigma=esd)
+    
+    ## generate beta1 matrix
+    Beta1<-matrix(0,nrow=m,ncol=p[1])
+    Beta1[,1]<-B.elem[1]
+    for(i in 1:2){
+      Beta1[((i-1)*m/2+1):(i*m/2),(1+(i-1)*2+1):(1+i*2)]<-B.elem[1]
+    }
+    for(i in 1:2){
+      Beta1[((i-1)*m/4+1):(i*m/4),(1+4*2+(i-1)*4+1):(1+4*2+i*4)]<-B.elem[1]
+    }
+    for(i in 1:4){
+      Beta1[((i-1)*m/8+1):(i*m/8),(1+2*2+4*4+(i-1)*8+1):(1+2*2+4*4+i*8)]<-B.elem[1]
+    }
+    ## generate beta2 matrix
+    Beta2<-matrix(0,nrow=m,ncol=p[2])
+    Beta2[,1]<-B.elem[2]
+    for(i in 1:2){
+      Beta2[((i-1)*m/2+1):(i*m/2),(1+(i-1)*2+1):(1+i*2)]<-B.elem[2]
+    }
+    for(i in 3:4){
+      Beta2[((i-1)*m/4+1):(i*m/4),(1+2*2+(i-1)*4+1):(1+2*2+i*4)]<-B.elem[2]
+    }
+    for(i in 5:8){
+      Beta2[((i-1)*m/8+1):(i*m/8),(1+2*2+4*4+(i-1)*8+1):(1+2*2+4*4+i*8)]<-B.elem[2]
+    }
+    Beta<-t(cbind(Beta1, Beta2))
+  }else{
+    cat("Ooops!!! Please specify 2-dim p vector, for example p=c(500,150)\n")
+  }
+  
+  Y<-X%*%Beta+e
+  return(list(Y=Y, X=X, Beta=Beta, e=e, p=p))
+}
+
+sim3 <- function(p=c(500,150),n=100,m=24,rho=.4,B.elem=c(0.2,0.6)){
+  b<-10
+  B.elem[1]<-0.2
+  B.elem[2]<-0.6
+  if(!is.na(p[2])){
+    # generate covariance matrix
+    Ap1<-matrix(rep(rho,(p[1]/b)^2),nrow=p[1]/b)
+    diag(Ap1)<-rep(1,p[1]/b)
+    Ap2<-matrix(rep(rho,(p[2]/b)^2),nrow=p[2]/b)
+    diag(Ap2)<-rep(1,p[2]/b)
+    Bp12<-matrix(rep(rho,p[1]/b*p[2]/b),nrow=p[1]/b)
+    Bp21<-matrix(rep(rho,p[1]/b*p[2]/b),nrow=p[2]/b)
+    Xsigma1<-Ap1
+    Xsigma2<-Ap2
+    Xsigma12<-Bp12
+    Xsigma21<-Bp21
+    for(i in 2:b){
+      Xsigma1<-bdiag(Xsigma1,Ap1)
+      Xsigma12<-bdiag(Xsigma12,Bp12)
+      Xsigma2<-bdiag(Xsigma2,Ap2)
+      Xsigma21<-bdiag(Xsigma21,Bp21)
+    }
+    
+    Xsigma<-rbind(cbind(Xsigma1,Xsigma12),cbind(Xsigma21,Xsigma2))
+    X<-	mvrnorm(n,mu=rep(0,p[1]+p[2]),Sigma=Xsigma)
+    X1<-X[,1:p[1]]
+    X2<-data.matrix(X[,(p[1]+1):(p[1]+p[2])] > 0) + 0
+    
+    # generate uncorrelated error term
+    esd<-diag(m)
+    e<-mvrnorm(n,mu=rep(0,m),Sigma=esd)
+    
+    ## generate two coefficient matrices
+    Beta1<-matrix(0,nrow=m,ncol=p[1])
+    Beta2<-matrix(0,nrow=m,ncol=p[2])
+    Beta1[1:5,5:6] <- B.elem[1]
+    Beta1[15:20,5:6] <- B.elem[1]
+    Beta1[3:6,10:11] <- B.elem[1]
+    Beta1[2:8,30:31] <- B.elem[1]
+    Beta1[16:21,30:31] <- B.elem[1]
+    Beta1[20:m,26:27] <- B.elem[1]
+    Beta1[7:13,90:91] <- B.elem[1]
+    Beta1[9:17,100:101] <- B.elem[1]
+    
+    Beta2[1:7,5:6] <- B.elem[2]
+    Beta2[5:6,7:8] <- B.elem[2]
+    Beta2[6:11,12:13] <- B.elem[2]
+    Beta2[8:9,14:15] <- B.elem[2]
+    Beta2[10:13,20:21] <- B.elem[2]
+    Beta2[14:15,26:27] <- B.elem[2]
+    Beta2[16:17,36:37] <- B.elem[2]
+    Beta2[18:19,46:47] <- B.elem[2]
+    Beta2[20:21,56:57] <- B.elem[2]
+    Beta2[22:23,66:67] <- B.elem[2]
+    Beta2[11:12,90:91] <- B.elem[2]
+    Beta2[3:14,100:101] <- B.elem[2]
+    Beta2[3:14,100:101] <- B.elem[2]
+    Beta2[13:18,110:111] <- B.elem[2]
+    Beta2[17:24,120:121] <- B.elem[2]
+    Beta <- t(cbind(Beta1, Beta2))
+  }else{
+    cat("Ooops!!! Please specify 2-dim p vector, for example p=c(500,150)\n")
+  }
+  
+  Y<-X%*%Beta+e
+  return(list(Y=Y, X=X, Beta=Beta, e=e, p=p))
+}
+
